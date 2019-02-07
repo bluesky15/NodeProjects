ae0d4440b775e9d0e7df0bb009e9a8b778c49c9e
Lalit Behera <lalit143.er@gmail.com>
2019-02-07 20:11:41 +0530

    program to hide the char of a string with X

--- src/index.js
+++ src/index.js
@@ -1,7 +1,5 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => res.send('Hello World!'))
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+function hideString(str){
+    return str.replace(/[a-zA-Z]/g,'X');
+}
+var hidden = hideString("hello World")
+console.log(hidden);
